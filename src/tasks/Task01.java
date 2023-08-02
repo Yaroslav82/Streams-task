c3c40bd093d74d8f66d36739bfec9b78e8c9b28c
Yaroslav82 <yargay230607@gmail.com>
2023-08-02 12:41:25 +0300

    Create print method

--- src/tasks/Task01.java
+++ src/tasks/Task01.java
@@ -1,8 +1,18 @@
 package tasks;
 
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class Task01 {
+
+    private static void printNames(Stream<String> names) {
+        AtomicInteger counter = new AtomicInteger(0);
+        names.forEach(name -> System.out.println(counter.incrementAndGet() + ") " + name));
+        if (counter.intValue() == 0) {
+            System.out.println("There is no name for this letter");
+        }
+    }
 
     private static Stream<String> filterNames(Stream<String> names, Character firstLetter) {
         return names.filter(name -> firstLetter.equals(name.charAt(0)));
