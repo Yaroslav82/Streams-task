38dd5d83d4e94fc135cad62aae241d87b192ead9
Yaroslav82 <yargay230607@gmail.com>
2023-08-02 12:22:47 +0300

    Create filterNames method

--- src/tasks/Task01.java
+++ src/tasks/Task01.java
@@ -4,6 +4,10 @@ import java.util.stream.Stream;
 
 public class Task01 {
 
+    private static Stream<String> filterNames(Stream<String> names, Character firstLetter) {
+        return names.filter(name -> firstLetter.equals(name.charAt(0)));
+    }
+
     private static Stream<String> getNames() {
         return Stream.of("Emma", "Liam", "Olivia", "Noah", "Ava", "Ethan", "Sophia", "Mason");
     }
