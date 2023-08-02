50a6bb486fde7bfe5592d8fdee36541d3749d3b8
Yaroslav82 <yargay230607@gmail.com>
2023-08-02 12:58:46 +0300

    Create getProducts method

---
+++ src/tasks/Task02.java
@@ -0,0 +1,10 @@
+package tasks;
+
+import java.util.Map;
+
+public class Task02 {
+
+    private static Map<String, Double> getProducts() {
+        return Map.of("pencil", 1.05, "notebook", 2.22, "book", 3.15, "pen", 1.75, "ruler", 2.00, "highlighter", 1.90);
+    }
+}
