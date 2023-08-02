40e9bb6cec2df0a780896da6fe9b980ee19dc1b1
Yaroslav82 <yargay230607@gmail.com>
2023-08-02 13:02:26 +0300

    Create filter method

--- src/tasks/Task02.java
+++ src/tasks/Task02.java
@@ -1,8 +1,13 @@
 package tasks;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Task02 {
+
+    private static Stream<String> filterProducts(Map<String, Double> products) {
+        return products.entrySet().stream().filter(product -> product.getValue() <= 2).map(Map.Entry::getKey);
+    }
 
     private static Map<String, Double> getProducts() {
         return Map.of("pencil", 1.05, "notebook", 2.22, "book", 3.15, "pen", 1.75, "ruler", 2.00, "highlighter", 1.90);
