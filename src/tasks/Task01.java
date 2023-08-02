c1d8d08b0469b1f0f02993e8618e4b9521e58ec3
Yaroslav82 <yargay230607@gmail.com>
2023-08-02 12:18:01 +0300

    Create getNames method

---
+++ src/tasks/Task01.java
@@ -0,0 +1,10 @@
+package tasks;
+
+import java.util.stream.Stream;
+
+public class Task01 {
+
+    private static Stream<String> getNames() {
+        return Stream.of("Emma", "Liam", "Olivia", "Noah", "Ava", "Ethan", "Sophia", "Mason");
+    }
+}
