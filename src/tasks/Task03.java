fe0b363589d593e0408edaa4457cdb113cc04703
Yaroslav82 <yargay230607@gmail.com>
2023-08-02 13:16:28 +0300

    Add filter method

--- src/tasks/Task03.java
+++ src/tasks/Task03.java
@@ -1,8 +1,15 @@
 package tasks;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Task03 {
+
+    private static Stream<String> filterForecast(Map<String, Integer> forecast) {
+        return forecast.entrySet().stream()
+                .filter(day -> day.getValue() >= 10 && day.getValue() <= 13)
+                .map(Map.Entry::getKey);
+    }
 
     private static Map<String, Integer> getForecast() {
         return Map.of("Monday", 8, "Tuesday", 9, "Wednesday", 10, "Thursday", 11, "Friday", 12, "Saturday", 13, "Sunday", 14);
