931d0db6e26fa4eae8b5ec691f029e381a536d19
Yaroslav82 <yargay230607@gmail.com>
2023-08-02 13:12:31 +0300

    Add getForecast method

---
+++ src/tasks/Task03.java
@@ -0,0 +1,10 @@
+package tasks;
+
+import java.util.Map;
+
+public class Task03 {
+
+    private static Map<String, Integer> getForecast() {
+        return Map.of("Monday", 8, "Tuesday", 9, "Wednesday", 10, "Thursday", 11, "Friday", 12, "Saturday", 13, "Sunday", 14);
+    }
+}
